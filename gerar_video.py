a81be2eeda91f810d009b33dbb6bcffe6627cc16
Micael Farias <Micaelfarias.dev@gmail.com>
2025-09-25 16:01:50 -0300

    up

--- gerar_video.py
+++ gerar_video.py
@@ -1,67 +1,92 @@
-# Importa as classes necessárias do moviepy
-from moviepy import VideoFileClip, TextClip, CompositeVideoClip, vfx, ImageClip
-from texto import gerar_texto
+from moviepy import VideoFileClip, CompositeVideoClip, vfx, ImageClip
 import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+
+def gerar_texto(dim: list, textos: list, font_path, font_size, cor, output, stroke_width=None, stroke_color=None, background_path=None):
+    w, h = dim
+    if background_path:
+        fundo = Image.open(background_path).convert("RGBA").resize((w, h))
+    else:
+        fundo = Image.new("RGBA", (w, h), (0, 0, 0, 0))
+
+    draw = ImageDraw.Draw(fundo)
+    fonte = ImageFont.truetype(font_path, font_size)
+
+    if len(textos) >= 1:
+        bbox1 = draw.textbbox(
+            (0, 0), textos[0], font=fonte, stroke_width=stroke_width)
+        text_w1, text_h1 = bbox1[2] - bbox1[0], bbox1[3] - bbox1[1]
+        x1 = (w - text_w1) // 2
+        y1 = 250
+        draw.text((x1, y1), textos[0], font=fonte, fill=cor,
+                  stroke_width=stroke_width, stroke_fill=stroke_color)
+
+    if len(textos) >= 2:
+        bbox2 = draw.textbbox(
+            (0, 0), textos[1], font=fonte, stroke_width=stroke_width)
+        text_w2, text_h2 = bbox2[2] - bbox2[0], bbox2[3] - bbox2[1]
+        x2 = (w - text_w2) // 2
+        y2 = h - text_h2 - 250
+        draw.text((x2, y2), textos[1], font=fonte, fill=cor,
+                  stroke_width=stroke_width, stroke_fill=stroke_color)
+
+    if output:
+        fundo.save(output)
+
+    return fundo
+
 
 def editar_video_com_texto(n):
     """
     """
-    video_path = f"parte_0{str(n).zfill(2)}.mp4"
-    font_path = "Brushot-Bold.ttf"
-    output_path = f"part{str(n).zfill(2)}.mp4"
-
-
-    print(f"Carregando vídeo de: {video_path}")
+    video_path = n['video']
+    init = 0
+    final = int(n['corte'])
     clip = VideoFileClip(video_path)
+    for_num = int(clip.duration / n['corte'])
+    target_width, target_height = n['dimensao']
+    for part in range(1, int(for_num) + 1):
+        output_path = f"parte-{str(part).zfill(2)}.mp4"
+        clip = VideoFileClip(video_path).subclipped(start_time=init, end_time=final)
+        clip_redimensionado = clip.with_effects(
+            [vfx.Resize(width=target_width)])
+        img_texto = gerar_texto(
+            [target_width, target_height],
+            [n['text'], f'Parte {part}'],
+            n['fonte'],
+            90,
+            'red',
+            output=None,
+            stroke_width=4,
+            stroke_color='white',
+            background_path=n['bg']
+        )
 
-    target_width = 720
-    target_height = 1280
+        frame = np.array(img_texto)
+        back = ImageClip(frame).with_duration(clip.duration)
+        video_final = CompositeVideoClip(
+            [back, clip_redimensionado.with_position("center")])
+        video_final.write_videofile(
+            output_path,
+            fps=30,
+            codec="libx264",
+            preset="ultrafast",
+            ffmpeg_params=["-tune", "fastdecode", "-crf", "28"]
+        )
+        init = final
+        final = int(final + n['corte'])
+    return print(f'{output_path} criado com sucesso')
 
-    print(
-        f"Redimensionando o vídeo para {target_width}x{target_height}...")
-    clip_redimensionado = clip.with_effects(
-        [vfx.Resize(width=target_width)])
-
-
-    print("Criando o clipe de texto...")
-    # Gera a imagem do texto (retorna PIL.Image)
-    img_texto = gerar_texto(
-        [target_width, target_height],
-        ['Gai vs Madara', f'Part {n}'],
-        'Brushot-Bold.ttf',
-        90,
-        'red',
-        output=None,  
-        stroke_width=4,
-        stroke_color='white'
-    )
-    frame = np.array(img_texto)
-
-    # Converte para ImageClip
-    back = ImageClip(frame).with_duration(clip.duration)
-
-
-    print("Combinando vídeo e texto...")
-    video_final = CompositeVideoClip([back, clip_redimensionado.with_position("center")])
-
-    print(f"Gerando o arquivo final: {output_path}...")
-    video_final.write_videofile(
-    output_path,
-    fps=30,
-    codec="libx264",
-    preset="ultrafast",
-    ffmpeg_params=["-tune", "fastdecode", "-crf", "28"]
-)
-
-
-    print(
-        f"\nEdição concluída com sucesso! O vídeo foi salvo como '{output_path}'.")
-
-
-
-
-from multiprocessing import Pool
 
 if __name__ == "__main__":
-    with Pool(1) as p:  # 4 processos em paralelo
-        p.map(editar_video_com_texto, range(6, 11))
+    data = {
+        'video': 'corte.mp4',
+        'fonte': 'Brushot-Bold.ttf',
+        'bg': 'image.png',
+        'dimensao': [720, 1280],
+        'corte': 120,
+        'text': 'Superman\n',
+    }
+
+    editar_video_com_texto(data)
